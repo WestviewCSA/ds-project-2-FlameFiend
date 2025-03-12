871b1d79abda7b9952f10a5b6d9022c3edbb5e6b
1930196 <1930196@01-18132.pusd.dom>
2025-03-12 13:31:00 -0700

    i think the reading works now i have to make the route :(

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class p2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("p2");
-	}
+    public static void main(String[] args) {
+        System.out.println("p2");
+    }
 
+    public static void readMap(String filename) {
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            int numRows = scanner.nextInt();
+            int numCols = scanner.nextInt();
+            int numRooms = scanner.nextInt();
+            scanner.nextLine();
+
+            Tile[][] map = new Tile[numRows][numCols];
+
+            int rowIndex = 0;
+            while (scanner.hasNextLine() && rowIndex < numRows) {
+                String row = scanner.nextLine();
+                for (int i = 0; i < numCols && i < row.length(); i++) {
+                    char el = row.charAt(i);
+                    map[rowIndex][i] = new Tile(rowIndex, i, el);
+                }
+                rowIndex++;
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
 }
