51b613782a7e63bff41b8cc63498ae22c5ffe70b
1930196 <1930196@01-18132.pusd.dom>
2025-03-13 13:39:53 -0700

    made a coordinate type test and starting the reading for coordinate reading

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -35,8 +35,38 @@ public class p2 {
             System.out.println("File not found!");
         }
     }
+    public static void coordRead(String filename) {
+    	try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            int numRows = scanner.nextInt();
+            int numCols = scanner.nextInt();
+            int numRooms = scanner.nextInt();
+            scanner.nextLine();
+
+            Tile[][] map = new Tile[numRows][numCols];
+
+            
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
     public static String queueRoute(String filename) {
     	readMap(filename);
     	
+    	return"";
+    }
+    public static String stackRoute(String filename) {
+    	readMap(filename);
+    	
+    	return"";
+    }
+    public static String optimalRoute(String filename) {
+    	readMap(filename);
+    	
+    	return"";
     }
 }
